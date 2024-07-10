188f7d1253e5cbafb6ccb5392d96c1857d27b9d6
gbortolini9 <119908312+gbortolini9@users.noreply.github.com>
2024-07-10 12:09:23 -0300

    gsdsdg

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function menuShow() {
         document.querySelector('.icon').src = "Imagens/cruz.png";
     }
 }
+
+const banner = document.getElementById('banner');
+const imagens = ['Imagens/bickel_top.png', 'Imagens/bickel_foto_superior2.png', 'Imagens/bickel_foto_superior3.png'];
+let index = 0;
+
+function alternarImagem() {
+    banner.style.backgroundImage = `url(${imagens[index]})`;
+    index = (index + 1) % imagens.length;
+}
+
+setInterval(alternarImagem, 5000); // Altera a imagem a cada 5 segundos
+
+// Inicia com a primeira imagem
+alternarImagem();
